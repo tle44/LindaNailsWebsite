3de739507df3b8514a816d84567c12cc3b6a074b
tle44 <134556232+tle44@users.noreply.github.com>
2024-04-07 23:54:47 -0500

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
         host: true,
-        // proxy: {
-        //     "/api": "http://localhost:5175",
-        // },
     },
 });
